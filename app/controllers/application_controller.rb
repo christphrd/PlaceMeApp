4ab2f79e1f2fb99e7d55202852f2d26525796916
github username <diep.christopher@gmail.com>
2018-05-27 21:37:30 -0400

    application controller updated. methods inherited

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  include SessionsHelper
+
+  def authorize_page
+    head(:forbidden) unless session[:user_id] == @user.id
+  end
+
+  def require_login
+    unless session.include? :user_id
+      redirect_to '/'
+    end
+  end
+
+  def get_user
+    @user = User.find(params[:id])
+  end
+
+  def session_user
+    @user = User.find(session[:user_id])
+  end
 end
